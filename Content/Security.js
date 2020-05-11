bffbd7ab7c544b07c0ec7bdebc677708a22ef8b3
citipk01 <62446172+citipk01@users.noreply.github.com>
2020-05-11 17:59:46 -0300

    Add files via upload

---
+++ Content/Security.js
@@ -0,0 +1,24 @@
+﻿
+function security_init(url) {
+    $(document).ready(function () {
+        // para todos los elementos raiz del menu tengo que buscar los items
+        $(".checkSecured").each(function (index) {
+            var inspectedElement = $(this);
+            var requiredCredential = $(this).attr("data-credential");
+            if (requiredCredential === null)
+                inspectedElement.remove();
+            $.ajax({
+                type: 'GET',
+                url: url,
+                data: { credential: requiredCredential },
+                dataType: 'json',
+                cache: true,
+                success: function (data) {
+                    if (data.status != 0) {
+                        inspectedElement.remove();
+                    }
+                }
+            });
+        });
+    });
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Buxis.Web.MvcApplication" Language="C#" %>
